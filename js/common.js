4b798b390df5fc03767e5b31df19ac2e0cd7afc8
ahasebe <aya.hasebe.7@gmail.com>
2019-06-24 16:12:50 +0900

    create modal

---
+++ js/common.js
@@ -0,0 +1,23 @@
+Vue.component('modal', {
+  template: '#modal-template'
+})
+
+// start app
+new Vue({
+  el: '#modal1',
+  data: {
+    showModal: false
+  }
+})
+
+// Vue.component('modal2', {
+//   template: '#modal-template2'
+// })
+
+// // start app
+// new Vue({
+//   el: '#modal2',
+//   data: {
+//     showModal2: false
+//   }
+// })
